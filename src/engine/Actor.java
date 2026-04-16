c1338e5e05ca250b42d0204f3fc90cf809e1a9bd
Jashna Oza <joza392@student.fuhsd.org>
2026-04-15 22:28:05 -0700

    updated the actor and world class

--- src/engine/Actor.java
+++ src/engine/Actor.java
@@ -2,6 +2,7 @@ package engine;
 
 import java.util.ArrayList;
 
+import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 
 public abstract class Actor extends ImageView{
@@ -18,26 +19,36 @@ public abstract class Actor extends ImageView{
 	}
 	
 	public double getHeight() {
-		return 0;
+		return getBoundsInLocal().getHeight();
 	}
 	
-	public <A extends Actor> java.util.List<A> getIntersectingObjects(){
-		return new ArrayList<A>();
+	public <A extends Actor> java.util.List<A> getIntersectingObjects(java.lang.Class<A> cls){
+		ArrayList<A> list = new ArrayList<>();
+		for (Node n : getParent().getChildrenUnmodifiable()) {
+			if (n != this && cls.isInstance(n)) {
+				Actor a = (Actor) n;
+				if (a.getBoundsInParent().intersects(this.getBoundsInParent())) {
+					list.add((A) a);
+				}
+			}
+		}
+		return list;
 	}
 	
-	public <A extends Actor> java.util.List<A> getOneIntersectingObject(){
+	public <A extends Actor> java.util.List<A> getOneIntersectingObject(java.lang.Class<A> cls){
 		return new ArrayList<A>();
 	}
 	
 	public double getWidth() {
-		return 0;
+		return getBoundsInLocal().getWidth();
 	}
 	
 	public World getWorld() {
-		return null;
+		return (World) getParent();
 	}
 	
 	public void move(double dx, double dy) {
-		
+		this.setX(this.getX() + dx);
+		this.setY(this.getY() + dy);
 	}
 }
--- src/engine/World.java
+++ src/engine/World.java
@@ -4,18 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javafx.animation.AnimationTimer;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 
 public abstract class World extends Pane{
-	private AnimationTimer myTimer;
+	private MyAnimationTimer myTimer;
 	private boolean isRunning;
-	ArrayList<KeyCode> keys = new ArrayList<>();
+	ArrayList<KeyCode> keys;
 	boolean widthSet;
 	boolean heightSet;
 	
 	public World() {
-		// widthProperty().addListener();
+		widthSet = false;
+		heightSet = false;
+		keys = new ArrayList<>();
+		isRunning = false;
+		HeightListener hListen = new HeightListener();
+		WidthListener wListen = new WidthListener();
+		widthProperty().addListener(wListen);
+		heightProperty().addListener(hListen);
+		SceneListener sListen = new SceneListener();
+		sceneProperty().addListener(sListen);
+		setOnKeyPressed(e -> {
+			keys.add(e.getCode());
+		});
+		
+		setOnKeyReleased(e -> {
+			keys.remove(e.getCode());
+		});
+		
+		myTimer = new MyAnimationTimer();
 	}
 	
 	public abstract void act(long now) ;
@@ -25,20 +46,40 @@ public abstract class World extends Pane{
 		actor.addedToWorld();
 	}
 	
-	public <A extends Actor> java.util.List<A> getObjects(){
-		return new ArrayList<A>();
+	public <A extends Actor> java.util.List<A> getObjects(java.lang.Class<A> cls){
+		ArrayList<A> actors = new ArrayList<A>();
+		for (A actor: actors) {
+			if (cls.isInstance(actor)) {
+				actors.add(actor);
+			}
+		}
+		return actors;
 	}
 	
-	public <A extends Actor> java.util.List<A> getObjectsAt(double x, double y){
-		return new ArrayList<A>();
+	public <A extends Actor> java.util.List<A> getObjectsAt(double x, double y, java.lang.Class<A> cls){
+		ArrayList<A> actors = new ArrayList<A>();
+		for (A actor: actors) {
+			if (cls.isInstance(actor)) {
+				if (actor.getX() == x && actor.getY() == y) {
+					actors.add(actor);
+				}
+			}
+		}
+		return actors;
 	}
 	
 	public boolean isKeyPressed(KeyCode code) {
+		if (keys.contains(code)){
+			return true;
+		}
 		return false;
 	}
 	
 	public boolean isStopped() { 
-		return false;
+		if (isRunning == true) {
+			return false;
+		}
+		return true;
 	}
 	
 	public abstract void onDimensionsInitialized();
@@ -52,6 +93,67 @@ public abstract class World extends Pane{
 	}
 	
 	public void stop() {
+		
+	}
+	
+	private class HeightListener implements ChangeListener<Number> {
+
+		@Override
+		public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+			if (oldValue.doubleValue() == 0 && newValue.doubleValue() > 0) {
+				heightSet = true;
+				
+			}
+			if (heightSet && widthSet) {
+				onDimensionsInitialized();
+			}
+			
+		}
+		
+	}
+	
+	private class WidthListener implements ChangeListener<Number> {
+
+		@Override
+		public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+			if (oldValue.doubleValue() == 0 && newValue.doubleValue() > 0) {
+				widthSet = true;
+				
+			}
+			if (heightSet && widthSet) {
+				onDimensionsInitialized();
+			}
+			
+		}
+		
+	}
+	
+	private class SceneListener implements ChangeListener<Scene>{
+
+		@Override
+		public void changed(ObservableValue<? extends Scene> observable, Scene oldScene, Scene newScene) {
+			if (newScene != null) {
+				requestFocus();
+			}
+			
+		}
+		
+	}
+	
+	private class MyAnimationTimer extends AnimationTimer {
+
+		@Override
+		public void handle(long now) {
+			act(now);
+			List<Actor> actorList = getObjects(Actor.class);
+			for (Actor a : actorList) {
+				if (getChildren().contains(a)) {
+					a.act(now);
+				}
+				
+			}
+			
+		}
 		
 	}
 	
