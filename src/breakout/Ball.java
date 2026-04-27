187ad61fe6c2b1e42400848fc5030d70f686a7a7
Jashna Oza <joza392@student.fuhsd.org>
2026-04-26 20:42:48 -0700

    added start pause

--- src/breakout/Ball.java
+++ src/breakout/Ball.java
@@ -2,6 +2,8 @@ package breakout;
 
 import engine.Actor;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 
 public class Ball extends Actor{
 
@@ -10,39 +12,54 @@ public class Ball extends Actor{
 	@Override
 	
 	public void act(long now) {
-		move(dx, dy);
-		if (getX() <= 0 || getX() + getWidth() >= getWorld().getWidth()) {
-			dx = -dx;
-		}
-		if (getY() <= 0 || getY() + getHeight() >= getWorld().getHeight()) {
-			dy = -dy;
-		}
 		
-		if (getY() + getHeight() >= getWorld().getHeight()) {
-			BallWorld w = (BallWorld) getWorld();
-			w.getScore().setScore(w.getScore().getScore() - 1000);
+		if (((BallWorld) getWorld()).getPaused() == true) { // If the world is paused
 			
-			w.getLives().setLives(w.getLives().getLives() - 1);
-		}
-		
-		if (getOneIntersectingObject(Paddle.class) != null) {
-			dy = -dy;
-		}
-		
-		if (getOneIntersectingObject(Brick.class) != null) {
-			Brick brick = getOneIntersectingObject(Brick.class);
-			if (getX() >= brick.getX() - brick.getWidth()/2 && getX() <= brick.getX() + brick.getWidth()/2) {
-				dy = -dy;
-			}else if (getY() >= brick.getY() - brick.getHeight()/2 && getY() <= brick.getY() + brick.getHeight()/2) {
+		}else {
+			
+
+			move(dx, dy);
+			if (getX() <= 0 || getX() + getWidth() >= getWorld().getWidth()) {
 				dx = -dx;
-			}else {
-				dx = -dx;
+			}
+			if (getY() <= 0 || getY() + getHeight() >= getWorld().getHeight()) {
 				dy = -dy;
 			}
 			
-			BallWorld w = (BallWorld) getWorld();
-			w.getScore().setScore(w.getScore().getScore() + 100);
-			w.remove(brick);
+			if (getY() + getHeight() >= getWorld().getHeight()) {
+				BallWorld w = (BallWorld) getWorld();
+				w.getScore().setScore(w.getScore().getScore() - 1000);
+				w.setPaused(true);
+				
+				
+				w.getLives().setLives(w.getLives().getLives() - 1);
+				
+				setX(getWorld().getWidth() / 2);
+			    setY(3 * getWorld().getHeight()/4 - 20);
+
+			    dx = 5;
+			    dy = -5;
+			}
+			
+			if (getOneIntersectingObject(Paddle.class) != null) {
+				dy = -dy;
+			}
+			
+			if (getOneIntersectingObject(Brick.class) != null) {
+				Brick brick = getOneIntersectingObject(Brick.class);
+				if (getX() >= brick.getX() - brick.getWidth()/2 && getX() <= brick.getX() + brick.getWidth()/2) {
+					dy = -dy;
+				}else if (getY() >= brick.getY() - brick.getHeight()/2 && getY() <= brick.getY() + brick.getHeight()/2) {
+					dx = -dx;
+				}else {
+					dx = -dx;
+					dy = -dy;
+				}
+				
+				BallWorld w = (BallWorld) getWorld();
+				w.getScore().setScore(w.getScore().getScore() + 100);
+				w.remove(brick);
+			}
 		}
 		
 	}
--- src/breakout/BallWorld.java
+++ src/breakout/BallWorld.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 import engine.World;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -18,6 +20,8 @@ public class BallWorld extends World{
 	private int level;
 	private Lives lives;
 	private Ball b;
+	private boolean isPaused = true;
+	private boolean lostShown = false;
 	
 	public BallWorld() {
 		setPrefSize(800, 600);
@@ -25,38 +29,16 @@ public class BallWorld extends World{
 
 	@Override
 	public void act(long now) {
-		if (getLives().getLives() <= 0) {
-			Text gameLost = new Text("Game Over! press any key to go back");
-			gameLost.setFont(new Font(20));
-			gameLost.setX(getWidth()/2);
-			gameLost.setY(getHeight()/2);
-			getChildren().add(gameLost);
-			setOnKeyPressed(e -> {
-				Stage s = (Stage) getScene().getWindow();
-				Breakout b = new Breakout();
-				try {
-					b.start(s);
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			});
-		}
-		boolean zeroBricks = true;
-		for(Node n: getChildren()) {
-			if (n instanceof Brick) {
-				zeroBricks = false;
-			}
-		}
 		
-		if (zeroBricks == true) {
-			level++;
-			if(level >=3) {
-				Text gameOver = new Text("Game Over! press any key to go back");
-				gameOver.setFont(new Font(20));
-				gameOver.setX(getWidth()/2);
-				gameOver.setY(getHeight()/2);
-				getChildren().add(gameOver);
+		
+		if (isPaused == false) {
+			if (getLives().getLives() <= 0 && lostShown == false) {
+				Text gameLost = new Text("Game Over! press any key to go back");
+				gameLost.setFont(new Font(20));
+				gameLost.setX(getWidth()/2);
+				gameLost.setY(getHeight()/2);
+				getChildren().add(gameLost);
+				lostShown = true;
 				setOnKeyPressed(e -> {
 					Stage s = (Stage) getScene().getWindow();
 					Breakout b = new Breakout();
@@ -67,35 +49,60 @@ public class BallWorld extends World{
 						e1.printStackTrace();
 					}
 				});
-				
-			}else {
-				b.setX(getWidth()/2);
-				b.setY(3 * getHeight()/4 - 20);
-				int r = 0;
-				int c = 0;
-				File f = new File("src/breakout/Level " + level + ".txt");
-				try {
-					Scanner s = new Scanner(f);
-					r = s.nextInt();
-					c = s.nextInt();
+			}
+			boolean zeroBricks = true;
+			for(Node n: getChildren()) {
+				if (n instanceof Brick) {
+					zeroBricks = false;
+				}
+			}
+			
+			if (zeroBricks == true) {
+				level++;
+				if(level >=3) {
+					Text gameOver = new Text("Game Over! press any key to go back");
+					gameOver.setFont(new Font(20));
+					gameOver.setX(getWidth()/2);
+					gameOver.setY(getHeight()/2);
+					getChildren().add(gameOver);
+					setOnKeyPressed(e -> {
+						Stage s = (Stage) getScene().getWindow();
+						Breakout b = new Breakout();
+						try {
+							b.start(s);
+						} catch (Exception e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					});
 					
-					
-					s.nextLine();
-					for (int i = 0; i < r; i++) {
-						String str = s.nextLine();
-						for (int j = 0; j < c; j++) {
-							char ch = str.charAt(j);
-							if (ch != '0') {
-								Brick brick = new Brick(ch);
-								add(brick);
-								brick.setX((getWidth() - (c * brick.getImage().getWidth())) / 2 + j * brick.getImage().getWidth());
-								brick.setY(100 + i * brick.getImage().getHeight());
+				}else {
+					int r = 0;
+					int c = 0;
+					File f = new File("src/breakout/Level " + level + ".txt");
+					try {
+						Scanner s = new Scanner(f);
+						r = s.nextInt();
+						c = s.nextInt();
+						
+						
+						s.nextLine();
+						for (int i = 0; i < r; i++) {
+							String str = s.nextLine();
+							for (int j = 0; j < c; j++) {
+								char ch = str.charAt(j);
+								if (ch != '0') {
+									Brick brick = new Brick(ch);
+									add(brick);
+									brick.setX((getWidth() - (c * brick.getImage().getWidth())) / 2 + j * brick.getImage().getWidth());
+									brick.setY(100 + i * brick.getImage().getHeight());
+								}
 							}
 						}
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
 				}
 			}
 		}
@@ -104,6 +111,19 @@ public class BallWorld extends World{
 
 	@Override
 	public void onDimensionsInitialized() {
+		
+		
+		setOnMousePressed(e -> {
+			if (e.getButton() == MouseButton.PRIMARY) {
+				isPaused = false;
+			}
+		});
+		setOnKeyPressed(e -> {
+		    if (e.getCode() == KeyCode.SPACE) {
+		        isPaused = false;
+		    }
+		});
+		
 		level = 1;
 		b = new Ball();
 		add(b);
@@ -160,7 +180,7 @@ public class BallWorld extends World{
 			e.printStackTrace();
 		}
 		
-		
+		requestFocus();
 		
 
 	}
@@ -171,6 +191,14 @@ public class BallWorld extends World{
 	
 	public Lives getLives() {
 		return lives;
+	}
+	
+	public boolean getPaused() {
+		return isPaused;
+	}
+	
+	public void setPaused(boolean paused) {
+		isPaused = paused;
 	}
 
 
